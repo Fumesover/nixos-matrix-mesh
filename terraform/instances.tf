6f4808e76ca3d89bb03e4894cd06bc87275e21ad
Albin Parou <albin.parou@exoscale.ch>
2026-06-22 20:16:40 +0200

    create 4 tuwunel instances

--- terraform/instances.tf
+++ terraform/instances.tf
@@ -2,6 +2,11 @@ locals {
   instances = {
     "nixos-host-1" = { zone = "ch-gva-2", type = "standard.medium", disk_size = 50, reverse_dns = "mail.parou.eu", security_group_ids = [ module.sg_mail.security_group_id ] }
     "nixos-host-2" = { zone = "ch-gva-2", type = "standard.medium", disk_size = 50, reverse_dns = "" }
+
+    "nixos-tuwumesh-1" = { zone = "ch-gva-2", type = "standard.small", disk_size = 10, reverse_dns = "" }
+    "nixos-tuwumesh-2" = { zone = "ch-dk-2",  type = "standard.small", disk_size = 10, reverse_dns = "" }
+    "nixos-tuwumesh-3" = { zone = "at-vie-1", type = "standard.small", disk_size = 10, reverse_dns = "" }
+    "nixos-tuwumesh-4" = { zone = "bg-sof-1", type = "standard.small", disk_size = 10, reverse_dns = "" }
   }
 }
 
