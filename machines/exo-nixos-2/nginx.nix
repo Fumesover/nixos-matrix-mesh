4928e36b60cec340e916bfcf130740171a4a1ff8
Albin Parou <albin.parou@exoscale.ch>
2026-06-22 22:01:31 +0200

    wip

--- machines/exo-nixos-2/nginx.nix
+++ machines/exo-nixos-2/nginx.nix
@@ -1,5 +1,62 @@
 { pkgs, lib, config, ... }:
+let
+  hostname = config.networking.hostName;
+  matrixDomain = "matrix.${hostname}.parou.eu";
 
+  elementConfig = pkgs.writeText "element-config.json" (builtins.toJSON {
+    default_server_config = {
+      "m.homeserver" = {
+        base_url = "https://${matrixDomain}";
+        server_name = matrixDomain;
+      };
+    };
+    disable_custom_urls = true;
+    disable_guests = true;
+    disable_login_language_selector = false;
+    disable_3pid_login = false;
+    force_verification = false;
+    brand = "Element";
+    integrations_ui_url = null;
+    integrations_rest_url = null;
+    integrations_widgets_urls = null;
+    default_widget_container_height = 280;
+    default_country_code = "CH";
+    show_labs_settings = false;
+    features = { };
+    default_federate = true;
+    default_theme = "light";
+    room_directory.servers = [ "matrix.org" ];
+    enable_presence_by_hs_url = {
+      "https://matrix.org" = false;
+      "https://matrix-client.matrix.org" = false;
+    };
+    setting_defaults = {
+      breadcrumbs = true;
+      "UIFeature.voip" = false;
+      "UIFeature.identityServer" = false;
+      "UIFeature.locationSharing" = false;
+    };
+    jitsi = null;
+    element_call = null;
+    map_style_url = "https://api.maptiler.com/maps/streets/style.json?key=fU3vlMsMn4Jb6dnEIFsx";
+  });
+
+  cinnyConfig = pkgs.writeText "cinny-config.json" (builtins.toJSON {
+    defaultHomeserver = 0;
+    homeserverList = [ matrixDomain ];
+    allowCustomHomeservers = false;
+    featuredCommunities = {
+      openAsDefault = false;
+      spaces = [ ];
+      rooms = [ ];
+      servers = [ ];
+    };
+    hashRouter = {
+      enabled = true;
+      basename = "/";
+    };
+  });
+in
 {
   services.nginx = {
     enable = true;
@@ -46,7 +103,7 @@
       };
 
       locations."= /config.json" = {
-        alias = ./cinny-config.json;
+        alias = cinnyConfig;
         extraConfig = ''
           default_type application/json;
         '';
@@ -63,7 +120,7 @@
       ];
 
       locations."= /config.json" = {
-        alias = ./element-config.json;
+        alias = elementConfig;
         extraConfig = ''
           default_type application/json;
         '';
